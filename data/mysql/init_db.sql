2a75e2a08781799cb14e3cd5995e43c69baf11e4
UsingCoding <extendedmoment@outlook.com>
2021-04-12 18:19:57 +0300

    Добавил sql для заполнения базы дефолтными таблицами и скрипт для ожидания подъёма базы

---
+++ data/mysql/init_db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS contentservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS userservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS playlistservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
